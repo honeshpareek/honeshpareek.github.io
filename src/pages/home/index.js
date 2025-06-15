b8a494990ab4d0f59ff8e7e644ddd88cf6df5419
honeshraipareek <honeshraipareek@gmail.com>
2025-06-16 00:15:55 +0530

    New Photographs in Photography page and Updated Homepage, Makde responsive

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -18,7 +18,7 @@ export const Home = () => {
         </Helmet>
         <div className="intro_sec d-block d-lg-flex align-items-center ">
           <div className="h_bg-image flex justify-center items-center order-1 order-lg-2 h-100">
-            <Eyes />
+            <Eyes className="eye_container" />
           </div>
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
             <div className="align-self-center ">
